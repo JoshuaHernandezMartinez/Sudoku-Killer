22f26cf0730d1011c6bd705293c1e67e9d26b0b5
JoshuaHernandezMartinez <johernandezma@unal.edu.co>
2017-01-05 16:38:07 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+// Solve the traditional 9x9 sudoku with the brute force algorithm
+// Developed by Joshua Hernandez
+// youtube channel: https://www.youtube.com/channel/UCgky3DZ3NHNTcwFw68aK8jQ
+
+
+public class Main {
+	public static void main(String[] args){
+		Display sudoku = new Display();
+		sudoku.setVisible(true);
+	}
+}
